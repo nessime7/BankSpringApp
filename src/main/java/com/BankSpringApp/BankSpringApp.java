496e926aadc22354fc67f65b172ed74c52e37b01
nessime7 <sara.przebinda@gmail.com>
2023-03-25 19:30:49 +0100

    version 1

---
+++ src/main/java/com/BankSpringApp/BankSpringApp.java
@@ -0,0 +1,12 @@
+package com.BankSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSpringApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankSpringApp.class, args);
+    }
+}
